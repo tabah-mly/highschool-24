3c3c1b8e037ceaa36b9545afe7c16991a483b1e2
tabah-mly <tabahmulya917@gmail.com>
2026-06-18 18:56:49 +0800

    chore: add additional codes

--- player.py
+++ player.py
@@ -2,13 +2,14 @@ import pygame
 from utils.player_base import PlayerBase
 from utils.spritesheet import SpriteSheet
 
+
 class Player(PlayerBase):
     def __init__(self, x, y):
         self.animations = {
-            "idle": ("assets/imgs/player_idle.png", 2, 0.1),
-            "walk": ("assets/imgs/player_walk.png", 2, 0.1),
-            "run": ("assets/imgs/player_run.png", 2, 0.07),
-            "attack": ("assets/imgs/player_attack.png", 2, 0.5, False),
+            "idle": SpriteSheet("assets/imgs/player_idle.png", 2, 0.1),
+            "walk": SpriteSheet("assets/imgs/player_walk.png", 2, 0.1),
+            "run": SpriteSheet("assets/imgs/player_run.png", 2, 0.07),
+            "attack": SpriteSheet("assets/imgs/player_attack.png", 2, 0.5, False),
         }
 
         self.stats = {
@@ -20,3 +21,15 @@ class Player(PlayerBase):
         }
 
         self.initialize(x, y)
+
+    def handle_input(self, dt):
+        keys = pygame.key.get_pressed()
+        direction = pygame.Vector2(keys[pygame.K_d] - keys[pygame.K_a], 0)
+        self.move(keys, direction, dt)
+
+    def update(self, dt):
+        self.handle_input(dt)
+        self.update_sprites(dt)
+
+    def draw(self, screen, camera):
+        self.draw_sprites(screen, camera)
